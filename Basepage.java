b811617a44913528b69c75774f653e4ca7c69d86
Dipak2381 <dipak_togadiya@yahoo.com>
2017-05-21 21:41:09 +0100

    Initial commit

---
+++ Basepage.java
@@ -0,0 +1,17 @@
+package com.nopcommerce;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * Created by Jay Vaghani on 20/05/2017.
+ */
+public class Basepage {
+    public static WebDriver driver;
+
+    Basepage()
+
+    {
+        PageFactory.initElements(driver, this);
+    }
+}
